97896585d562260d06736067692e786e263c8757
thgtvcaptain <88418059+thgtvhe7031@users.noreply.github.com>
2023-02-06 22:52:22 +0700

    Update index.js

--- follabj_fe/src/index.js
+++ follabj_fe/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import "./style/flexboxgrid.min.css";
 import './style/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from "react-redux"
+import store, {persistor} from './Redux/store';
+import { PersistGate } from 'redux-persist/integration/react'
+import {BrowserRouter as Router} from 'react-router-dom';
 
-
-ReactDOM.render(
-  <>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <App />
-  </>,
-  document.getElementById('root')
+    </PersistGate> 
+    </Provider>
 );
-
 
 
 
