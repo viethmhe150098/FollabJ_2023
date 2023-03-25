aba91d08e0337c306e5699a7ce1fc6e9e7b0d878
giangnvthe <giangnvthe150748@fpt.edu.vn>
2023-03-25 23:18:49 +0700

    Revert "Revert "edit Toast & FE for admin managements""
    This reverts commit 83136fe44889dd83b0fb0219418cf9d721ece1a4.

--- follabj_fe/src/Redux/auth/apiRequest.js
+++ follabj_fe/src/Redux/auth/apiRequest.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 import { async } from "q";
 import jwtDecode from 'jwt-decode';
-import {loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./authSlice";
+import { loginFailed, loginStart, loginSuccess, registerFailed, registerStart, registerSuccess } from "./authSlice";
 import { createProjectFailed, createProjectStart, createProjectSuccess, getProjectStart } from "../project/projectSlice";
+import { toast } from "react-toastify";
 
-
-export const loginUser = async(user,dispatch,navigate) =>{
+export const loginUser = async (user, dispatch, navigate) => {
     dispatch(loginStart());
     try {
         const res = await axios.post("http://localhost:8080/login",
-                                        user,
-                                        {
-                                            headers: {
-                                                'Content-Type': 'application/x-www-form-urlencoded'
-                                              }
-                                        });
+            user,
+            {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                }
+            });
 
         //console.log(res.data);
         const access_token = res.data.access_token;
@@ -23,25 +23,26 @@ export const loginUser = async(user,dispatch,navigate) =>{
         //console.log("access_token ", res.data.access_token);
         localStorage.setItem("refresh_token", refresh_token);
         //console.log("refresh_token", res.data.refresh_token);
-        
-        localStorage.setItem("expire_date", new Date().getTime() + 10*60*1000)
+
+        localStorage.setItem("expire_date", new Date().getTime() + 10 * 60 * 1000)
 
         const decodedToken = jwtDecode(localStorage.getItem('access_token'));
         //console.log('decoded access token '+JSON.stringify(decodedToken));
-        
+
         // const decodedRefreshToken = jwtDecode(localStorage.getItem('refresh_token'));
         // console.log('decoded refresh token '+JSON.stringify(decodedRefreshToken));
 
         const role_name = decodedToken.roles;
-        
+
         localStorage.setItem("role_name", role_name);
-        
+
         //console.log("role_name", role_name);
 
 
         //console.log(res)
         dispatch(loginSuccess(res.data));
-        // go to home page
+        // go to home page'
+        toast.success('Welcome back! 🧡',)
         navigate.push("/")
     } catch (error) {
         //console.log(error)
@@ -49,49 +50,44 @@ export const loginUser = async(user,dispatch,navigate) =>{
     }
 }
 
-export const registerUser = async(user,dispatch,navigate) =>{
+export const registerUser = async (user, dispatch, navigate) => {
     dispatch(registerStart());
     try {
         await axios.post("http://localhost:8080/signup",
-                                       user,
-                                        {
-                                            // headers: {                                            
-                                                //     'Content-Type': 'application/x-www-form-urlencoded'
-                                            // }
-                                        });
+            user,
+            {
+                // headers: {                                            
+                //     'Content-Type': 'application/x-www-form-urlencoded'
+                // }
+            });
         dispatch(registerSuccess());
+        toast.success('Register successfully, please CHECK your email!', {
+            autoClose: 10000
+        })
         navigate.push("/login");
     } catch (error) {
         dispatch(registerFailed());
     }
 }
 
-// export const getAllProjects = async (access_token,dispatch) =>{
-//     dispatch(getProjectStart());
-//     try {
-//         const res = await axios.get 
-//     } catch (error) {
-        
-//     }
-// }
 
-    export const createProject = async(project,access_token,dispatch,navigate) =>{
-        dispatch(createProjectStart());
+export const createProject = async (project, access_token, dispatch, navigate) => {
+    dispatch(createProjectStart());
 
-        const role_name = localStorage.getItem("role_name");
-        if (role_name.includes('LEADER')) {
-            try {
-                const res = await axios.post("http://localhost:8080/createproject",
-                                                project,
-                                                {
-                                                    headers: {
-                                                        Authorization: `Bearer ${access_token}`
-                                                    }
-                                                });
-                 dispatch(createProjectSuccess(res.data));
-                 navigate.push("/aboutProject");
-             } catch (error) {
-                dispatch(createProjectFailed());
-             }
+    const role_name = localStorage.getItem("role_name");
+    if (role_name.includes('LEADER')) {
+        try {
+            const res = await axios.post("http://localhost:8080/createproject",
+                project,
+                {
+                    headers: {
+                        Authorization: `Bearer ${access_token}`
+                    }
+                });
+            dispatch(createProjectSuccess(res.data));
+            navigate.push("/aboutProject");
+        } catch (error) {
+            dispatch(createProjectFailed());
         }
-    }    
+    }
+}    
