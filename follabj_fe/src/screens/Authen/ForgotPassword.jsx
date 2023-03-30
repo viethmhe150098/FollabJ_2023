f4602a9e61ccaaebb54d45ad2dd5b2620952c529
BuiHai1502 <122037456+haibdhe141497@users.noreply.github.com>
2023-03-30 15:48:20 +0700

    forgot password (done)

--- follabj_fe/src/screens/Authen/ForgotPassword.jsx
+++ follabj_fe/src/screens/Authen/ForgotPassword.jsx
@@ -8,15 +8,30 @@ import FullButton from "../../components/Buttons/FullButton";
 import AuthenNavbar from "../../components/Nav/AuthenNavbar";
 import { useState, useEffect } from 'react';
 import { toast } from "react-toastify";
+import axios from "axios";
 
 const ForgotPassword = () => {
 
+  const [email, setEmail] = useState('');
 
 
   // Regex to validate email
   const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
   // Regex to validate password
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8080/user/forgot", { email })
+      .then((response) => {
+        console.log(response.data);
+        alert(response.data.message); 
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+        alert(error.response.data.message); 
+      });
+  }
   
   return (
     <>
@@ -24,9 +39,11 @@ const ForgotPassword = () => {
       <div className="Wrapper">
         <div className="auth-form-container">
           <h2>Set Your Password</h2>
-          <form className="login-form">
+          <form className="login-form" onSubmit={handleSubmit}>
             <label className="semiBold font15" htmlFor="email">Email</label>
-            <input type="text" className="input" placeholder="Enter your username" required/>
+            <input type="text" className="input" placeholder="Enter your username" required 
+            onChange={(e) => setEmail(e.target.value)}
+            />
             <div style={{ marginTop: '10px ' }}></div>
             <FullButton title="Get Started" />
           </form>
