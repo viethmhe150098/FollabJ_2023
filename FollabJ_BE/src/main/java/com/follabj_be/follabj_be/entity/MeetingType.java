f2c8dc88ac05e6f5b327436e765862d825bbde37
viethmhe150098 <viethmhe150098@fpt.edu.vn>
2023-04-16 22:14:17 +0700

    BE constraint

--- FollabJ_BE/src/main/java/com/follabj_be/follabj_be/entity/MeetingType.java
+++ FollabJ_BE/src/main/java/com/follabj_be/follabj_be/entity/MeetingType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -16,7 +17,7 @@ import javax.persistence.Id;
 public class MeetingType {
     @Id
     private Integer id;
-
+    @Column(nullable = false, unique = true)
     private String type;
 
 
